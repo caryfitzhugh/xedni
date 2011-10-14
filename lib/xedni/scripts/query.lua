8b787b325e1582ff57e092f485c23f33d6a07364
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2011-10-14 12:02:06 -0400

    Logging on redis.

--- lib/xedni/scripts/query.lua
+++ lib/xedni/scripts/query.lua
@@ -1,5 +1,9 @@
 local args = xedni.unpack(ARGV[1]);
+print('query input', table.show(args));
 local results = xedni.search.query(args.query);
+print('query results', table.show(results));
 results = xedni.search.sort(results, args.weights);
+print('sorted results', table.show(results));
 results = xedni.search.paginate(results, args.options);
+print('paginated results', table.show(results));
 return xedni.pack(results);
--- lib/xedni/scripts/read.lua
+++ lib/xedni/scripts/read.lua
@@ -1,3 +1,4 @@
 local args = xedni.unpack(ARGV[1]);
 local record_data = xedni.records.read(args.record);
+print('reading', table.show(record_data));
 return xedni.pack(record_data);
