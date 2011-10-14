446e3298565868cd9c289d8c1dfe3090aef427ab
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2011-10-14 12:03:50 -0400

    ...

--- lib/xedni/scripts/read.lua
+++ lib/xedni/scripts/read.lua
@@ -1,4 +1,5 @@
 local args = xedni.unpack(ARGV[1]);
+print('reading', table.show(args));
 local record_data = xedni.records.read(args.record);
 print('reading', table.show(record_data));
 return xedni.pack(record_data);
